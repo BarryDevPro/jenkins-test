93784e2b5b2e79496648304ee5ea6e3e8a10d71b
Barry <47219965+BarryDevPro@users.noreply.github.com>
2021-08-18 13:43:10 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("******** Jenkins *****************");
-        for(int i = 1 ; i < 20 ; i++){
+        for(int i = 1 ; i <= 20 ; i++){
             System.out.println("Numero ===> "+i);
         }
         System.out.println("******** fin Jenkins *****************");
