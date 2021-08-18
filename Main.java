e7d49d078ea528337612c1293f2a41067905ba4d
BarryDevPro <wouryndi@gmail.com>
2021-08-18 12:57:53 +0000

    project test jenkins

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("******** Jenkins *****************");
+        for(int i = 1 ; i < 10 ; i++){
+            System.out.println("Numero ===> "+i);
+        }
+        System.out.println("******** end Jenkins *****************");
+
+    }
+}
